83bd5b0fb0481e3c76578e27d2a59bd52278ee14
auzbuzzard <auzbuzzard@gmail.com>
2018-02-24 11:53:52 -0800

    Utterances for image search (derpi) is now in one message with line break

--- botservice/imagesearch.py
+++ botservice/imagesearch.py
@@ -22,13 +22,15 @@ class ImageSearch:
         image, count = derpibooru.search(args, tags)
 
         if image is not None:
-            await self.bot.say("Found image for: {} (from {} results)".format(tags, count))
-            await self.bot.say("https:" + image.representations['large'])
+            await self.bot.say(
+                "Found image for: {} (from {} results) ".format(tags, count) +
+                "https:{}".format(image.representations['large'])
+            )
         else:
             await self.bot.say("Can't find images for: {}. :<".format(tags))
 
-    @commands.command(name='derpi', pass_context=True, no_pm=False)
-    async def search_derpi(self, ctx, *, inputs: str):
+    @commands.command(name='e621', pass_context=True, no_pm=False)
+    async def search_e621(self, ctx, *, inputs: str):
 
         args, tags = derpibooru.parse_args(inputs)
 
